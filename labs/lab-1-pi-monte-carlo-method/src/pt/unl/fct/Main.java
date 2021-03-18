c4935986f3833feb29727295971ae962d2925c44
Rúben André Letra Barreiro <r.barreiro@campus.fct.unl.pt>
2021-03-18 09:58:48 +0000

    Some minor changes in the main of the program added

--- labs/lab-1-pi-monte-carlo-method/src/pt/unl/fct/Main.java
+++ labs/lab-1-pi-monte-carlo-method/src/pt/unl/fct/Main.java
@@ -3,6 +3,16 @@ package pt.unl.fct;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        if(args.length == 0 || args.length > 2) {
+
+            // If the program does not receive the expected arguments,
+            // it will print an error message, providing a suggestion of the
+            // arguments needed
+            System.err.println("Usage: pi-monte-carlo-method program_version num_points [num_threads]");
+
+        }
+
     }
+
 }
