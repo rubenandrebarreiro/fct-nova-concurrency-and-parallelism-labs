b05b49716707cfa464867b9199fb3e21de635458
rubenandrebarreiro <ruben.andre.letra.barreiro@gmail.com>
2021-04-09 20:42:07 +0100

    CMake for Lab #3 (Pi Approximation by Monte Carlo Method) added

---
+++ labs/lab-3-pi-approximation-monte-carlo-method-in-open-mp/cmake-build-debug/CMakeFiles/lab_3_pi_approximation_monte_carlo_method_in_open_mp.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/rubenandrebarreiro/git/fct-nova-concurrency-and-parallelism-labs/labs/lab-3-pi-approximation-monte-carlo-method-in-open-mp/main.c" "/home/rubenandrebarreiro/git/fct-nova-concurrency-and-parallelism-labs/labs/lab-3-pi-approximation-monte-carlo-method-in-open-mp/cmake-build-debug/CMakeFiles/lab_3_pi_approximation_monte_carlo_method_in_open_mp.dir/main.c.o"
+  "/home/rubenandrebarreiro/git/fct-nova-concurrency-and-parallelism-labs/labs/lab-3-pi-approximation-monte-carlo-method-in-open-mp/parallel_version/pi_approximation_monte_carlo_parallel_version.c" "/home/rubenandrebarreiro/git/fct-nova-concurrency-and-parallelism-labs/labs/lab-3-pi-approximation-monte-carlo-method-in-open-mp/cmake-build-debug/CMakeFiles/lab_3_pi_approximation_monte_carlo_method_in_open_mp.dir/parallel_version/pi_approximation_monte_carlo_parallel_version.c.o"
+  "/home/rubenandrebarreiro/git/fct-nova-concurrency-and-parallelism-labs/labs/lab-3-pi-approximation-monte-carlo-method-in-open-mp/sequential_version/pi_approximation_monte_carlo_sequential_version.c" "/home/rubenandrebarreiro/git/fct-nova-concurrency-and-parallelism-labs/labs/lab-3-pi-approximation-monte-carlo-method-in-open-mp/cmake-build-debug/CMakeFiles/lab_3_pi_approximation_monte_carlo_method_in_open_mp.dir/sequential_version/pi_approximation_monte_carlo_sequential_version.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
