47168f89208cf4f6e78ca932a9c39db6864719c9
rubenandrebarreiro <ruben.andre.letra.barreiro@gmail.com>
2021-04-09 20:37:45 +0100

    Sequential Version for the Pi Approximation by Monte Carlo Method in C added

---
+++ labs/lab-2-pi-approximation-monte-carlo-method-in-c/sequential_version/pi_approximation_monte_carlo_sequential_version.h
@@ -0,0 +1,22 @@
+/**
+ *
+ * Pi Approximation/Estimation, for the Monte Carlo Method,
+ * using Sequential and Parallel Versions (Version in C)
+ *
+ * @author: Ruben Andre Barreiro
+ * @email: r.barreiro@campus.fct.unl.pt
+ * @number_student: 42648
+ *
+ * NOVA School of Science and Technology | NOVA University of Lisbon (FCT NOVA)
+ *
+ */
+
+#ifndef LAB_2_PI_APPROXIMATION_MONTE_CARLO_METHOD_IN_C_PI_APPROXIMATION_MONTE_CARLO_SEQUENTIAL_VERSION_H
+#define LAB_2_PI_APPROXIMATION_MONTE_CARLO_METHOD_IN_C_PI_APPROXIMATION_MONTE_CARLO_SEQUENTIAL_VERSION_H
+
+void *sequential_pi_approximation(const long *arg_num_points);
+
+int print_final_results_sequential_pi_approximation(const long *arg_num_points,
+                                                    const long *arg_inside_hits_count);
+
+#endif //LAB_2_PI_APPROXIMATION_MONTE_CARLO_METHOD_IN_C_PI_APPROXIMATION_MONTE_CARLO_SEQUENTIAL_VERSION_H
