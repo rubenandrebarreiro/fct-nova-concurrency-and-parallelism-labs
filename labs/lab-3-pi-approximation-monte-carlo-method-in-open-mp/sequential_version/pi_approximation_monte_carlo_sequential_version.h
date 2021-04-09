f21a0ac916594a19d7eb11ce208d8716841b2862
rubenandrebarreiro <ruben.andre.letra.barreiro@gmail.com>
2021-04-09 20:43:47 +0100

    Sequential Version for the Pi Approximation by Monte Carlo Method in OpenMP added

---
+++ labs/lab-3-pi-approximation-monte-carlo-method-in-open-mp/sequential_version/pi_approximation_monte_carlo_sequential_version.h
@@ -0,0 +1,22 @@
+/**
+ *
+ * Pi Approximation/Estimation, for the Monte Carlo Method,
+ * using Sequential and Parallel Versions (Version in OpenMP)
+ *
+ * @author: Ruben Andre Barreiro
+ * @email: r.barreiro@campus.fct.unl.pt
+ * @number_student: 42648
+ *
+ * NOVA School of Science and Technology | NOVA University of Lisbon (FCT NOVA)
+ *
+ */
+
+#ifndef LAB_3_PI_APPROXIMATION_MONTE_CARLO_METHOD_IN_OPEN_MP_PI_APPROXIMATION_MONTE_CARLO_SEQUENTIAL_VERSION_H
+#define LAB_3_PI_APPROXIMATION_MONTE_CARLO_METHOD_IN_OPEN_MP_PI_APPROXIMATION_MONTE_CARLO_SEQUENTIAL_VERSION_H
+
+void *sequential_pi_approximation(const long *arg_num_points);
+
+int print_final_results_sequential_pi_approximation(const long *arg_num_points,
+                                                    const long *arg_inside_hits_count);
+
+#endif //LAB_3_PI_APPROXIMATION_MONTE_CARLO_METHOD_IN_OPEN_MP_PI_APPROXIMATION_MONTE_CARLO_SEQUENTIAL_VERSION_H
