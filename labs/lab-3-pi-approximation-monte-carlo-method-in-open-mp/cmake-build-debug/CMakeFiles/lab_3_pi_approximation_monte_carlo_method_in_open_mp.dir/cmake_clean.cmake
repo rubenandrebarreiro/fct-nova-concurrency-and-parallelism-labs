1093a6a786278a3a473b5c9d9a96e4e293b20c21
rubenandrebarreiro <ruben.andre.letra.barreiro@gmail.com>
2021-04-09 20:42:04 +0100

    CMake for Lab #3 (Pi Approximation by Monte Carlo Method) added

---
+++ labs/lab-3-pi-approximation-monte-carlo-method-in-open-mp/cmake-build-debug/CMakeFiles/lab_3_pi_approximation_monte_carlo_method_in_open_mp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab_3_pi_approximation_monte_carlo_method_in_open_mp.dir/main.c.o"
+  "CMakeFiles/lab_3_pi_approximation_monte_carlo_method_in_open_mp.dir/parallel_version/pi_approximation_monte_carlo_parallel_version.c.o"
+  "CMakeFiles/lab_3_pi_approximation_monte_carlo_method_in_open_mp.dir/sequential_version/pi_approximation_monte_carlo_sequential_version.c.o"
+  "lab_3_pi_approximation_monte_carlo_method_in_open_mp"
+  "lab_3_pi_approximation_monte_carlo_method_in_open_mp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lab_3_pi_approximation_monte_carlo_method_in_open_mp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
