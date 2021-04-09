1cd20b09e8ec14484d6fc72831c3bda83dce3156
rubenandrebarreiro <ruben.andre.letra.barreiro@gmail.com>
2021-04-09 20:35:50 +0100

    New Project for the Lab #2 (Pi Approximation in C) created

---
+++ labs/lab-2-pi-approximation-monte-carlo-method-in-c/cmake-build-debug/CMakeFiles/lab_2_pi_approximation_monte_carlo_method_in_c.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/rubenandrebarreiro/CLionProjects/lab-2-pi-approximation-monte-carlo-method-in-c/main.c" "/home/rubenandrebarreiro/CLionProjects/lab-2-pi-approximation-monte-carlo-method-in-c/cmake-build-debug/CMakeFiles/lab_2_pi_approximation_monte_carlo_method_in_c.dir/main.c.o"
+  "/home/rubenandrebarreiro/CLionProjects/lab-2-pi-approximation-monte-carlo-method-in-c/parallel_version/pi_approximation_monte_carlo_parallel_version.c" "/home/rubenandrebarreiro/CLionProjects/lab-2-pi-approximation-monte-carlo-method-in-c/cmake-build-debug/CMakeFiles/lab_2_pi_approximation_monte_carlo_method_in_c.dir/parallel_version/pi_approximation_monte_carlo_parallel_version.c.o"
+  "/home/rubenandrebarreiro/CLionProjects/lab-2-pi-approximation-monte-carlo-method-in-c/sequential_version/pi_approximation_monte_carlo_sequential_version.c" "/home/rubenandrebarreiro/CLionProjects/lab-2-pi-approximation-monte-carlo-method-in-c/cmake-build-debug/CMakeFiles/lab_2_pi_approximation_monte_carlo_method_in_c.dir/sequential_version/pi_approximation_monte_carlo_sequential_version.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ labs/lab-2-pi-approximation-monte-carlo-method-in-c/cmake-build-debug/CMakeFiles/lab_2_pi_approximation_monte_carlo_method_in_c.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab_2_pi_approximation_monte_carlo_method_in_c.dir/main.c.o"
+  "CMakeFiles/lab_2_pi_approximation_monte_carlo_method_in_c.dir/parallel_version/pi_approximation_monte_carlo_parallel_version.c.o"
+  "CMakeFiles/lab_2_pi_approximation_monte_carlo_method_in_c.dir/sequential_version/pi_approximation_monte_carlo_sequential_version.c.o"
+  "lab_2_pi_approximation_monte_carlo_method_in_c"
+  "lab_2_pi_approximation_monte_carlo_method_in_c.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lab_2_pi_approximation_monte_carlo_method_in_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
