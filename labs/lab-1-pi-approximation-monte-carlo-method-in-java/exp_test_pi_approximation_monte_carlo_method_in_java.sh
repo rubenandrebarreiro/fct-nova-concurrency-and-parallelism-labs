94f03f3d17c8380cb692cc05025c2cfafedec6dd
rubenandrebarreiro <ruben.andre.letra.barreiro@gmail.com>
2021-04-09 20:26:45 +0100

    New Shell File for automated tests added

---
+++ labs/lab-1-pi-approximation-monte-carlo-method-in-java/exp_test_pi_approximation_monte_carlo_method_in_java.sh
@@ -0,0 +1,24 @@
+NUM_POINTS="1000 10000 100000 1000000 10000000 100000000 1000000000"
+NUM_THREADS="1 2 4 8 12"
+NUM_RUNS="5"
+
+for NP in $NUM_POINTS; do
+  for NR in $(seq 1 $NUM_RUNS); do
+    # shellcheck disable=SC2028
+    echo "Run/Execution no. $NR:"$'\n'
+    time java -jar pi_approximation_monte_carlo_method_in_java.jar sequential "$NP"
+    echo $'\n\n'
+  done
+done | tee pi_approximation_monte_carlo_method_in_java_sequential_results.csv
+
+for NP in $NUM_POINTS; do
+  for NT in $NUM_THREADS; do
+    # shellcheck disable=SC2034
+    for NR in $(seq 1 $NUM_RUNS); do
+      # shellcheck disable=SC2028
+      echo "Run/Execution no. $NR:"$'\n'
+      time java -jar pi_approximation_monte_carlo_method_in_java.jar parallel "$NP" "$NT"
+      echo $'\n\n'
+    done
+  done
+done | tee pi_approximation_monte_carlo_method_in_java_parallel_results.csv
