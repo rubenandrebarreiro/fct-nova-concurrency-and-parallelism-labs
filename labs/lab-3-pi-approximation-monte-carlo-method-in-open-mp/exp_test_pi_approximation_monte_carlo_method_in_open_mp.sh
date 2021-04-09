33e43b784911bfde38eb2b55e2916c53d4566284
rubenandrebarreiro <ruben.andre.letra.barreiro@gmail.com>
2021-04-09 20:44:38 +0100

    Shell File for Automated Tests added

---
+++ labs/lab-3-pi-approximation-monte-carlo-method-in-open-mp/exp_test_pi_approximation_monte_carlo_method_in_open_mp.sh
@@ -0,0 +1,24 @@
+NUM_POINTS="1000 10000 100000 1000000 10000000 100000000 1000000000"
+NUM_THREADS="1 2 4 8 12"
+NUM_RUNS="5"
+
+for NP in $NUM_POINTS; do
+  for NR in $(seq 1 $NUM_RUNS); do
+    # shellcheck disable=SC2028
+    echo "Run/Execution no. $NR:"$'\n'
+    time ./pi_approximation_monte_carlo_method_in_open_mp sequential "$NP"
+    echo $'\n\n'
+  done
+done | tee pi_approximation_monte_carlo_method_in_open_mp_sequential_results.csv
+
+for NP in $NUM_POINTS; do
+  for NT in $NUM_THREADS; do
+    # shellcheck disable=SC2034
+    for NR in $(seq 1 $NUM_RUNS); do
+      # shellcheck disable=SC2028
+      echo "Run/Execution no. $NR:"$'\n'
+      time ./pi_approximation_monte_carlo_method_in_open_mp parallel "$NP" "$NT"
+      echo $'\n\n'
+    done
+  done
+done | tee pi_approximation_monte_carlo_method_in_open_mp_parallel_results.csv
