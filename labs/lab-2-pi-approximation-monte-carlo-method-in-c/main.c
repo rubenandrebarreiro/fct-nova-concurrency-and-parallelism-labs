ec0a42f71c76e0eba1de8bd2e489d61331226e07
rubenandrebarreiro <ruben.andre.letra.barreiro@gmail.com>
2021-04-10 02:00:36 +0100

    Some minor changes on the Exercise of the Lab #2

--- labs/lab-2-pi-approximation-monte-carlo-method-in-c/main.c
+++ labs/lab-2-pi-approximation-monte-carlo-method-in-c/main.c
@@ -29,6 +29,7 @@
 #define ERROR_USAGE_MESSAGE_3 "       num-points: The number of points to be generated on the simulation\n"
 #define ERROR_USAGE_MESSAGE_4 "       [num_threads]: The number of Threads to be launched in Parallel Version (if none is specified, 1 Thread will be used, as default) [OPTIONAL - only specifiable for the Parallel Versions]\n"
 
+// Print the Error Message for Wrong Arguments' Usage
 int print_error_arguments_messages() {
 
     fprintf(stderr, "%s", ERROR_WRONG_ARGS_MESSAGE);
@@ -37,102 +38,125 @@ int print_error_arguments_messages() {
     fprintf(stderr, "%s", ERROR_USAGE_MESSAGE_3);
     fprintf(stderr, "%s", ERROR_USAGE_MESSAGE_4);
 
+    // Return from the function
     return 1;
 
 }
 
+// The Main Method, with the Command-Line Interface
 int main(const int argc, const char *argv[]) {
 
+    // If it were given 4 arguments, at most, in the prompt of the Command-Line Interface
     if(argc <= 4) {
 
+        // The Simulation Version (i.e., Sequential or Parallel)
         char *simulation_version;
-
         simulation_version = (char *) malloc(strlen(argv[1]));
-
         strcpy(simulation_version, argv[1]);
 
+        // Convert the Simulation Version, to Lower Case
         for(int char_index = 0; simulation_version[char_index]; char_index++) {
 
             simulation_version[char_index] = tolower(simulation_version[char_index]); // NOLINT(cppcoreguidelines-narrowing-conversions)
 
         }
 
+        // If the Simulation Version chosen is the Sequential
         if(strcasecmp(simulation_version, SEQUENTIAL_VERSION_TAG) == 0) {
 
+            // If it were given 3 arguments, in the prompt of the Command-Line Interface
             if(argc == 3) {
 
+                // The number of the Points to be generated for the Simulation
                 long *num_points;
-
                 num_points = (long *) malloc(sizeof(long));
-
                 *num_points = atol(argv[2]); // NOLINT(cert-err34-c)
 
+                // The number of Hit Counts, within the Circle
                 long *inside_hit_counts;
-
                 inside_hit_counts = (long *) malloc(sizeof(long));
 
+                // Execute the Simulation, in Sequential, and retrieve the number of Hit Counts, within the Circle
                 *inside_hit_counts = *(long *) sequential_pi_approximation(num_points);
 
+                // Print the Final Results, for the Simulation, in Sequential
                 print_final_results_sequential_pi_approximation(num_points, inside_hit_counts);
 
             }
             else{
 
+                // Print the Error Messages for the Wrong Arguments' usage
                 print_error_arguments_messages();
+
+                // Exit the Program, with the Error Code
                 exit(1);
 
             }
 
         }
+        // If the Simulation Version chosen is the Parallel
         else if(strcasecmp(simulation_version, PARALLEL_VERSION_TAG) == 0)  {
 
+            // If it were given 3 or 4 arguments, in the prompt of the Command-Line Interface
             if( (argc == 3) || (argc == 4)) {
 
+                // The number of the Points to be generated for the Simulation
                 long *num_points;
-
                 num_points = (long *) malloc(sizeof(long));
-
                 *num_points = atol(argv[2]); // NOLINT(cert-err34-c)
 
+                // The number of Threads for the Parallel Version of the Simulation
                 int *num_threads;
-
                 num_threads = (int *) malloc(sizeof(int));
 
+                // If it were given 3 arguments, in the prompt of the Command-Line Interface
                 if(argc == 3) {
 
+                    // Print the Error Message, related with a bad usage of the arguments, for the number of Threads
                     printf("WARNING: The number of Threads was not specified!!!\n");
                     printf("         It will be used the minimum number of Threads (i.e., %d Thread), as argument of the Simulation...\n\n", 1);
 
+                    // Set the number of Threads to be used, as 1
                     *num_threads = 1;
 
                 }
                 else {
 
+                    // Retrieve the number of Threads to be used, from the prompt of the Command-Line
                     *num_threads = atoi(argv[3]); // NOLINT(cert-err34-c)
 
+                    // If the number of Threads, chosen, via the prompt of the Command-Line,
+                    // is greater than the number of Processors available
                     if(*num_threads > get_nprocs()) {
 
+                        // Print the Error Message, related with a bad usage of the arguments, for the number of Threads
                         printf("WARNING: The number of Threads specified is greater than the number of Threads available!!!\n");
                         printf("         It will be used the number of Threads available (i.e., %d Thread(s)), as argument of the Simulation...\n\n", get_nprocs());
 
+                        // Set the number of Threads to be used, as the maximum number of Processors available
                         *num_threads = get_nprocs();
 
                     }
 
                 }
 
+                // The number of Hit Counts, within the Circle
                 long *inside_hit_counts;
-
                 inside_hit_counts = (long *) malloc(sizeof(long));
 
+                // Execute the Simulation, in Parallel, and retrieve the number of Hit Counts, within the Circle
                 *inside_hit_counts = *(long *) parallel_pi_approximation(num_points, num_threads);
 
+                // Print the Final Results, for the Simulation, in Parallel
                 print_final_results_parallel_pi_approximation(num_points, num_threads, inside_hit_counts);
 
             }
             else{
 
+                // Print the Error Messages for the Wrong Arguments' usage
                 print_error_arguments_messages();
+
+                // Exit the Program, with the Error Code 1
                 exit(1);
 
             }
@@ -140,17 +164,24 @@ int main(const int argc, const char *argv[]) {
         }
         else {
 
+            // Print the Error Messages for the Wrong Arguments' usage
             print_error_arguments_messages();
+
+            // Exit the Program, with the Error Code 1
             exit(1);
 
         }
 
+        // Return from the Main Method
         return 0;
 
     }
     else {
 
+        // Print the Error Messages for the Wrong Arguments' usage
         print_error_arguments_messages();
+
+        // Exit the Program, with the Error Code 1
         exit(1);
 
     }
--- labs/lab-2-pi-approximation-monte-carlo-method-in-c/sequential_version/pi_approximation_monte_carlo_sequential_version.c
+++ labs/lab-2-pi-approximation-monte-carlo-method-in-c/sequential_version/pi_approximation_monte_carlo_sequential_version.c
@@ -15,59 +15,76 @@
 #include <stdio.h>
 #include <time.h>
 
+// The Circle has Radius of 1
 #define CIRCLE_RADIUS 1
 
+// The Method for the Pi Approximation, by the Sequential Version
 void *sequential_pi_approximation(const long *arg_num_points) {
 
+    // The total number of Points, to be generated, during the Simulation
     long *total_num_points;
     total_num_points = (long *) malloc(sizeof(long));
     *total_num_points = *arg_num_points;
 
+    // The number of Hit Counts, within the Circle
     long *inside_hits_count;
     inside_hits_count = (long *) malloc(sizeof(long));
     *inside_hits_count = 0;
 
+    // Initialise the Seed for the Pseudo-Random Generator
     srandom(time(NULL));
 
+    // For each Point to be generated, during the Simulation
     for(int current_point = 0; current_point < *total_num_points; current_point++) {
 
+        // Generate the random coordinates for the Point, using the Pseudo-Random Generator
         float point_x = ( ( (float) random() ) / (float)(RAND_MAX) );
         float point_y = ( ( (float) random() ) / (float)(RAND_MAX) );
 
+        // Compute the Pythagoras formula
         double pythagoras_formula = ( (point_x * point_x) + (point_y * point_y) );
 
+        // If the Point currently generated, is within the Circle
         if( pythagoras_formula <= CIRCLE_RADIUS ) {
 
+            // Increment the number of Hit Counts, within the Circle
             *inside_hits_count += 1;
 
         }
 
     }
 
+    // Return the number of Hit Counts, within the Circle
     return (void *) inside_hits_count;
 
 }
 
+// The method for printing the Results of the Pi Approximation, by the Sequential Version
 int print_final_results_sequential_pi_approximation(const long *arg_num_points,
                                                     const long *arg_inside_hits_count) {
 
+    // The total number of Points, for the Simulation
     long *total_num_points;
     total_num_points = (long *) malloc(sizeof(long));
     *total_num_points = *arg_num_points;
 
+    // The number of Points, within the Circle
     long *inside_hits_count;
     inside_hits_count = (long *) malloc(sizeof(long));
     *inside_hits_count = *arg_inside_hits_count;
 
+    // The Pi Estimation, by the computation of the Points within the Circle
     double *pi_estimation;
     pi_estimation = (double *) malloc(sizeof(double));
     *pi_estimation = (double) ( 4 * ( (float) ( ( (double) *inside_hits_count ) / ( (double) *total_num_points ) ) ) );
 
+    // Print the Final Results
     printf("--- Pi Approximation (Monte Carlo Method) [Sequential] ---\n");
     printf("Total Number of Points: %ld\n", *total_num_points);
     printf("Points within Circle: %ld\n", *inside_hits_count);
     printf("Pi Estimation: %1.5f\n", *pi_estimation);
 
+    // Return from the function
     return 0;
 
 }
